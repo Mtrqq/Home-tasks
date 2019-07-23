da838dced115c5247e7406d8e0dbbb6194869d21
mtrqq <maxym.fugol@gmail.com>
2019-07-23 16:36:04 +0300

    replaced new char[] for member in derived block

--- SharedPointer/SharedControlBlock.h
+++ SharedPointer/SharedControlBlock.h
@@ -11,20 +11,13 @@ namespace nostd
 	class SharedControlBlockBase
 	{
 	public:
-		SharedControlBlockBase(TValueType* i_pointer)
-			:mp_data{ i_pointer },
-			m_shared_count{},
+		SharedControlBlockBase()
+			:m_shared_count{ 1 },
 			m_weak_count{} {}
 
-		const TValueType* get() const
-		{
-			return mp_data;
-		}
+		virtual const TValueType* get() const = 0;
 
-		TValueType* get()
-		{
-			return mp_data;
-		}
+		virtual TValueType* get() = 0;
 
 		size_t& weak_ptr_count()
 		{
@@ -38,7 +31,7 @@ namespace nostd
 
 		bool is_valid() const
 		{
-			return mp_data != nullptr;
+			return get() != nullptr;
 		}
 
 		virtual void destroyObject() = 0;
@@ -46,64 +39,76 @@ namespace nostd
 		virtual ~SharedControlBlockBase() {}
 
 	protected:
-
-		template <typename ...Args>
-		SharedControlBlockBase(Args && ... arguments)
-			:mp_data{ new(this + 1)TValueType{std::forward<TValueType>(arguments)...}},
-			m_shared_count{},
-			m_weak_count{} {}
-
 		size_t m_shared_count;
 		size_t m_weak_count;
-		TValueType* mp_data;
-
-		template<typename TValueType, typename ...Args>
-		friend SharedPointer<TValueType> make_shared(Args &&... args);
 	};
 
 	template<typename TValueType>
-	struct BlindedBlock : public SharedControlBlockBase<TValueType>
+	class BlindedBlock : public SharedControlBlockBase<TValueType>
 	{
+	public:
 		template <typename ...Args>
 		BlindedBlock(Args && ... arguments)
-			:SharedControlBlockBase<TValueType>{ std::forward<TValueType>(arguments)... }
+			:data{std::forward<Args>(arguments)...}
 		{}
 
 		virtual void destroyObject() override
 		{
 			std::cout << "Object destroyed" << std::endl;
-			this->mp_data->~TValueType();
-			this->mp_data = nullptr;
+			data.~TValueType();
 		}
 
 		virtual ~BlindedBlock() override
 		{
 			std::cout << "Full Destroy" << std::endl;
-			::operator delete[](this);
 		}
 
+		virtual const TValueType* get() const
+		{
+			return &data;
+		}
+
+		virtual TValueType* get()
+		{
+			return &data;
+		}
+
+	private:
+		TValueType data;
 	};
 
 	template<typename TValueType>
-	struct RemoteBlock : public SharedControlBlockBase<TValueType>
+	class RemoteBlock : public SharedControlBlockBase<TValueType>
 	{
+	public:
 		RemoteBlock(TValueType* i_pointer)
-			:SharedControlBlockBase<TValueType>{ i_pointer }
+			:data{i_pointer}
 		{}
 
 		virtual void destroyObject() override
 		{
 			std::cout << "Object destroyed" << std::endl;
-			delete this->mp_data;
-			this->mp_data = nullptr;
+			delete data;
+			data = nullptr;
 		}
 
 		virtual ~RemoteBlock() override
 		{
 			std::cout << "Full Destroy" << std::endl;
-			if (this->mp_data) 
-				delete this->mp_data;
-			::operator delete(this);
+			if (data) delete data;
 		}
+
+		virtual const TValueType* get() const
+		{
+			return data;
+		}
+
+		virtual TValueType* get()
+		{
+			return data;
+		}
+
+	private:
+		TValueType* data;
 	};
 }
